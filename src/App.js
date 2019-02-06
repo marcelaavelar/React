42266f329a82dd8b4d374506d2b15b3e373a164a
Marcela Avelar <marcela.lilian@gmail.com>
2019-02-06 16:06:01 -0200

    Atualizado

--- src/App.js
+++ src/App.js
@@ -7,24 +7,52 @@ class App extends Component {
 
   constructor() {
     super();
-    this.state = {lista : []};
+    this.state = {lista : [], nome:'', email:'', senha:''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
   }
-    componentDidMount(){
-      $.ajax({
-        url:"http://cdc-react.herokuapp.com/api/autores",
-        dataType:'json',
-        success:function(resposta) {
-          this.setState({lista:resposta});
-        }.bind(this)
-      }
-    );
+
+  componentDidMount(){
+    $.ajax({
+      url:"http://cdc-react.herokuapp.com/api/autores",
+      dataType:'json',
+      success:function(resposta) {
+        this.setState({lista:resposta});
+      }.bind(this)
+    });
   }
 
   enviaForm(evento) {
-    evento.preventDefaul();
-    console.log("dados sendo enviados"); 
+    evento.preventDefault();
+
+    $.ajax({
+      url:"http://cdc-react.herokuapp.com/api/autores",
+      contentType:'application/json',
+      dataType:'json',
+      type:'post',
+      data:JSON.stringify({nome:this.state.nome, email:this.state.email, senha:this.state.senha}),
+      success: function(resposta) {
+        this.setState({lista:resposta});
+      }.bind(this),
+      error: function(resposta) {
+        console.log("erro");
+      }
+    });
   }
 
+  setNome(evento) {
+    this.setState({nome:evento.target.value});
+  }
+
+  setEmail(evento) {
+    this.setState({email:evento.target.value});
+  }
+
+  setSenha(evento) {
+    this.setState({senha:evento.target.value});
+  }
   render() {
     return (
 
@@ -50,16 +78,16 @@ class App extends Component {
             <div className="pure-form pure-form-aligned">
               <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
                 <div className="pure-control-group">
-                  <label for="nome">Nome</label> 
-                  <input id="nome" type="text" name="nome" value=""  />                  
+                  <label htmlFor="nome">Nome</label> 
+                  <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />                  
                 </div>
                 <div className="pure-control-group">
-                  <label for="email">Email</label> 
-                  <input id="email" type="email" name="email" value=""  />                  
+                  <label htmlFor="email">Email</label> 
+                  <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />                  
                 </div>
                 <div className="pure-control-group">
-                  <label for="senha">Senha</label> 
-                  <input id="senha" type="password" name="senha"  />                                      
+                  <label htmlFor="senha">Senha</label> 
+                  <input id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />                                      
                 </div>
                 <div className="pure-control-group">                                  
                   <label></label> 
